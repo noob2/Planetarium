e148cc43c27b418ca1a18edc3ec896012b16351b
dim4o <viktorproto3@gmail.com>
2021-03-19 15:13:32 +0200

    fix collisions

--- app/engine.js
+++ app/engine.js
@@ -7,7 +7,7 @@ let zoom = 1
 
 document.getElementById('clear').addEventListener('click', () => (objects = []))
 
-canvas.addEventListener('contextmenu', (e) => e.preventDefault())
+canvas.addEventListener('contextmenu', e => e.preventDefault())
 
 let startPosition = { x: 0, y: 0 }
 let currentPosition = { x: 0, y: 0 }
@@ -25,7 +25,7 @@ function dragOffset(e) {
   currentPosition.y = e.clientY
 }
 
-canvas.addEventListener('mousedown', (e) => {
+canvas.addEventListener('mousedown', e => {
   startPosition = { x: e.clientX, y: e.clientY }
   currentPosition = { x: e.clientX, y: e.clientY }
   if (e.button == 0) {
@@ -35,7 +35,7 @@ canvas.addEventListener('mousedown', (e) => {
     canvas.addEventListener('mousemove', dragOffset, false)
   }
 })
-canvas.addEventListener('mouseup', (e) => {
+canvas.addEventListener('mouseup', e => {
   if (e.button == 0) {
     doIDraw = false
     canvas.removeEventListener('mousemove', drawLine, false)
@@ -45,13 +45,13 @@ canvas.addEventListener('mouseup', (e) => {
       x: (e.clientX + offset.x) / zoom,
       y: (e.clientY + offset.y) / zoom,
       xSpeed: (currentPosition.x - startPosition.x) / 20,
-      ySpeed: (currentPosition.y - startPosition.y) / 20,
+      ySpeed: (currentPosition.y - startPosition.y) / 20
     })
   } else {
     canvas.removeEventListener('mousemove', dragOffset, false)
   }
 })
-canvas.addEventListener('wheel', (e) => {
+canvas.addEventListener('wheel', e => {
   let direction = e.deltaY > 0 ? 'down' : 'up'
   direction == 'down' ? (zoom *= 2) : (zoom /= 2)
   document.getElementById('zoom').value = zoom
@@ -69,21 +69,21 @@ setInterval(() => {
         if (Math.sqrt(distanceSquared) > o2.radius + o.radius) {
           const object1Gravity = o.mass / distanceSquared
           const object2Gravity = o2.mass / distanceSquared
+          const gravitationalReduction = document.getElementById('gravitationReduction').value
+          o.xSpeed += (object2Gravity / gravitationalReduction) * (o2.x - o.x)
+          o2.xSpeed += (object1Gravity / gravitationalReduction) * (o.x - o2.x)
 
-          o.xSpeed += (object2Gravity / 100) * (o2.x - o.x)
-          o2.xSpeed += (object1Gravity / 100) * (o.x - o2.x)
-
-          o.ySpeed += (object2Gravity / 100) * (o2.y - o.y)
-          o2.ySpeed += (object1Gravity / 100) * (o.y - o2.y)
+          o.ySpeed += (object2Gravity / gravitationalReduction) * (o2.y - o.y)
+          o2.ySpeed += (object1Gravity / gravitationalReduction) * (o.y - o2.y)
         } else {
           //crash
           objects[i] = {
-            radius: (o.radius + o2.radius) / 2,
+            radius: Math.sqrt((Math.PI * o.radius * o.radius + Math.PI * o2.radius * o2.radius) / Math.PI),
             mass: o.mass + o2.mass,
             x: (o.x + o2.x) / 2,
             y: (o.y + o2.y) / 2,
-            xSpeed: 0,
-            ySpeed: 0,
+            xSpeed: (o.xSpeed * o.mass + o2.xSpeed * o2.mass) / (o.mass + o2.mass),
+            ySpeed: (o.ySpeed * o.mass + o2.ySpeed * o2.mass) / (o.mass + o2.mass)
           }
           objects.splice(j, 1)
         }
